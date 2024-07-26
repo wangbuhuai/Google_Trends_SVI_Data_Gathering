8b440c6c367c90bff90ea5a296b8d59fde38a79d
Dayu Wang <dwang@stchas.edu>
2024-07-25 22:25:43 -0500

    progress 1

--- server.js
+++ server.js
@@ -10,13 +10,46 @@ const app = express();
 
 // Setup views and middleware.
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/style'))
+app.use(express.static(__dirname + '/static'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Global values
+const errorMessages = {
+    'api-key': [false, '[Error] A valid API key is required.'],
+    'database-file': [false, '[Error] A valid database file is required.'],
+    'column': [false, '[Error] A valid column is required.']
+};
+
+const getSviData = {
+    'api-key': '',
+    'column': ''
+};
+
 app.get('/', (_, res) => {
-    res.render('index');
+    res.render('index', {
+        getSviData: getSviData,
+        errorMessages: errorMessages
+    });
+});
+
+app.post('/', (req, res) => {
+    const apiKey = req.body.apiKey;
+    const databaseFile = req.body.databaseFile;
+    const column = req.body.column;
+
+    errorMessages['api-key'][0] = (getSviData['api-key'] = apiKey.trim()) === '';
+    errorMessages['database-file'][0] = databaseFile.trim() === '';
+    errorMessages['column'][0] = (getSviData['column'] = column.trim()) === '';
+
+    if (errorMessages['api-key'][0] || errorMessages['database-file'][0] || errorMessages['column'][0]) {
+        res.render('index', {
+            getSviData: getSviData,
+            errorMessages: errorMessages
+        });
+    } else {
+    }
 });
 
 app.listen(3000);
-console.log('Server is listening to port 3000');
+console.log('Server is running on http://localhost:3000');
---
+++ static/js/client.js
@@ -0,0 +1,30 @@
+// Created by Dayu Wang (dwang@stchas.edu) on 2024-07-25
+
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2024-07-25
+
+
+let path = '';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const apiKeyLabelElement = document.getElementById('api-key-label');
+    const width1 = apiKeyLabelElement.getBoundingClientRect().width;
+    const apiKeyErrorElement = document.getElementById('api-key-error');
+    apiKeyErrorElement.style.paddingLeft = (width1 + 5).toString() + 'px';
+
+    const databaseFileLabelElement = document.getElementById('database-file-label');
+    const width2 = databaseFileLabelElement.getBoundingClientRect().width;
+    const csvRequirementsElement = document.getElementById('csv-requirements');
+    csvRequirementsElement.style.paddingLeft = (width2 + 5).toString() + 'px';
+    const databaseFileErrorElement = document.getElementById('database-file-error');
+    databaseFileErrorElement.style.paddingLeft = (width2 + 5).toString() + 'px';
+
+    const columnLabelElement = document.getElementById('column-label');
+    const width3 = columnLabelElement.getBoundingClientRect().width;
+    const columnRequirementsElement = document.getElementById('column-requirements');
+    columnRequirementsElement.style.paddingLeft = (width3 + 5).toString() + 'px';
+    const columnErrorElement = document.getElementById('column-error');
+    columnErrorElement.style.paddingLeft = (width3 + 5).toString() + 'px';
+
+    document.getElementById('get-svi').addEventListener('click', () => {
+    });
+});
